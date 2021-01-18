a9848d0086e821908942e5f86bdc884a11db80ba
parkyes90 <parkyes90@gmail.com>
2021-01-18 22:52:11 +0900

    bind input event

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
   methods: {
+    setName(e) {
+      this.name = e.target.value;
+    },
     add(number) {
       this.counter += number;
     },
