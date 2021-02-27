bfbb09f988d4b6b36b9620d8fc903e15c4db9f87
parkyes90 <parkyes90@gmail.com>
2021-02-27 23:17:05 +0900

    chore: Apply style scope

--- module-app/src/components/UserInfo.vue
+++ module-app/src/components/UserInfo.vue
@@ -19,13 +19,19 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 section {
   margin: 2rem auto;
   max-width: 30rem;
   border-radius: 12px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   padding: 1rem;
+}
+
+section header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 section div {
