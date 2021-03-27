49b4256f594474bc4f94e66e49eeadc27e961897
parkyes90 <parkyes90@gmail.com>
2021-03-27 23:35:21 +0900

    feature: Add dynamic components & keep alive

--- module-app/src/App.vue
+++ module-app/src/App.vue
@@ -13,6 +13,11 @@
         <p>{{ slotProps["another-prop"] }}</p>
       </template>
     </course-goals>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <keep-alive>
+      <component :is="selectedComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
@@ -21,21 +26,31 @@ import TheHeader from "./components/TheHeader";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
 import CourseGoals from "@/components/CourseGoals";
+import ActiveGoals from "@/components/ActiveGoals";
+import ManageGoals from "@/components/ManageGoals";
 export default {
   components: {
     TheHeader,
     BadgeList,
     UserInfo,
     CourseGoals,
+    ActiveGoals,
+    ManageGoals,
   },
   data() {
     return {
+      selectedComponent: "active-goals",
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
         role: "admin",
       },
     };
+  },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp;
+    },
   },
 };
 </script>
