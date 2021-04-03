20b29aca7423349b9775ba0845d547938a8a3f27
parkyes90 <parkyes90@gmail.com>
2021-04-03 20:52:49 +0900

    Multiple Routes with Name Router views

--- routing/src/main.js
+++ routing/src/main.js
@@ -5,6 +5,8 @@ import TeamMembers from '@/components/teams/TeamMembers';
 import TeamsList from '@/components/teams/TeamsList';
 import UsersList from '@/components/users/UsersList';
 import NotFound from '@/components/nav/NotFound';
+import TeamsFooter from '@/components/teams/TeamsFooter';
+import UsersFooter from '@/components/users/UsersFooter';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -16,7 +18,10 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
-      component: TeamsList,
+      components: {
+        default: TeamsList,
+        footer: TeamsFooter,
+      },
       children: [
         {
           name: 'team-members',
@@ -28,7 +33,10 @@ const router = createRouter({
     },
     {
       path: '/users',
-      component: UsersList,
+      components: {
+        default: UsersList,
+        footer: UsersFooter,
+      },
     },
     { path: '/:notFound(.*)', component: NotFound },
   ],
