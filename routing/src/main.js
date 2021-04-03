a7b59affdc738d54fa31c150e8f5a40a4bc3d772
parkyes90 <parkyes90@gmail.com>
2021-04-03 19:04:17 +0900

    feature: Add Nested route

--- routing/src/main.js
+++ routing/src/main.js
@@ -16,12 +16,12 @@ const router = createRouter({
     {
       path: '/teams',
       component: TeamsList,
+      children: [{ path: ':teamId', component: TeamMembers, props: true }],
     },
     {
       path: '/users',
       component: UsersList,
     },
-    { path: '/teams/:teamId', component: TeamMembers, props: true },
     { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
