3baafe0744264f8463345a79b50dba16f912bbca
parkyes90 <parkyes90@gmail.com>
2021-04-05 21:52:02 +0900

    feature: connect store

--- global-state/src/main.js
+++ global-state/src/main.js
@@ -1,7 +1,16 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex';
+
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+});
 
 import App from './App.vue';
 
 const app = createApp(App);
-
+app.use(store);
 app.mount('#app');
