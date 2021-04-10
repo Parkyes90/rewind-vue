68070ee5e882783407c36bc5e3d1c6e26b86956d
parkyes90 <parkyes90@gmail.com>
2021-04-10 22:41:41 +0900

    feature: Add Coaches List and List Items

--- find-coach/src/pages/coaches/CoachesList.vue
+++ find-coach/src/pages/coaches/CoachesList.vue
@@ -6,16 +6,24 @@
       <router-link to="/register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filterCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
+      <coach-item
+        v-for="coach in filterCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      ></coach-item>
     </ul>
     <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from '@/components/coaches/CoachItem';
 export default {
+  components: { CoachItem },
   computed: {
     filterCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -26,3 +34,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
