0f1e5355b37f14239aa38193a4d0d0b7645dc247
parkyes90 <parkyes90@gmail.com>
2021-04-11 20:55:11 +0900

    feature: Add Coach Form

--- find-coach/src/pages/coaches/CoachRegister.vue
+++ find-coach/src/pages/coaches/CoachRegister.vue
@@ -1,5 +1,19 @@
-<template>REFISTER</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form></coach-form>
+    </base-card>
+  </section>
+</template>
 
 <script>
-export default {};
+import CoachForm from '@/pages/coaches/CoachForm';
+import BaseCard from '@/components/ui/BaseCard';
+export default {
+  components: {
+    BaseCard,
+    CoachForm,
+  },
+};
 </script>
