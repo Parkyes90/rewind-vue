59f3553eccaa3780bd1ec978303a66b0612f05e9
parkyes90 <parkyes90@gmail.com>
2021-04-14 22:44:49 +0900

    feature: Add with firebase

--- find-coach/src/store/modules/coaches/actions.js
+++ find-coach/src/store/modules/coaches/actions.js
@@ -1,9 +1,18 @@
 export default {
   registerCoach(context, data) {
-    context.commit('registerCoach', {
-      id: context.rootGetters.userId,
-      userId: context.rootGetters.userId,
+    const userId = context.rootGetters.userId;
+    const coach = {
+      userId,
       ...data,
-    });
+    };
+    fetch(`https://vuejs-http-96326.firebaseio.com/coaches/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coach),
+    })
+      .then((res) => res.json())
+      .then((json) => context.commit('registerCoach', { id: userId, ...json }))
+      .catch((err) => {
+        console.log(err);
+      });
   },
 };
