a9eabb9fee35f3295d461524f5ac2a76490130dc
parkyes90 <parkyes90@gmail.com>
2021-04-15 23:36:54 +0900

    feature: Requests with firebase

--- find-coach/src/pages/requests/RequestReceived.vue
+++ find-coach/src/pages/requests/RequestReceived.vue
@@ -1,32 +1,61 @@
 <template>
+  <base-dialog :show="!!error" title="An error occurred!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests Received</h2>
-        <ul v-if="hasRequests">
-          <request-item
-            v-for="req in receivedRequests"
-            :key="req.id"
-            :email="req.email"
-            :message="req.message"
-          ></request-item>
-        </ul>
-        <h3 v-else>You haven't received any requests yet!</h3>
       </header>
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequests && !isLoading">
+        <request-item
+          v-for="req in receivedRequests"
+          :key="req.id"
+          :email="req.email"
+          :message="req.message"
+        ></request-item>
+      </ul>
+      <h3 v-else>You haven't received any requests yet!</h3>
     </base-card>
   </section>
 </template>
 <script>
 import BaseCard from '@/components/ui/BaseCard';
+import BaseDialog from '@/components/ui/BaseDialog';
 import RequestItem from '@/components/requests/RequestItem';
+import BaseSpinner from '@/components/ui/BaseSpinner';
 export default {
-  components: { RequestItem, BaseCard },
+  components: { RequestItem, BaseCard, BaseDialog, BaseSpinner },
+  data() {
+    return {
+      isLoading: false,
+      error: null,
+    };
+  },
   computed: {
     receivedRequests() {
       return this.$store.getters['requests/requests'];
     },
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
+    },
+  },
+  created() {
+    this.loadRequests();
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        this.$store.dispatch('requests/fetchRequests');
+      } catch (e) {
+        this.error = e.message || 'Something failed';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     },
   },
 };
--- find-coach/src/store/modules/requests/actions.js
+++ find-coach/src/store/modules/requests/actions.js
@@ -1,8 +1,36 @@
 export default {
-  contactCoach(context, payload) {
-    context.commit('addRequest', {
-      id: new Date().toISOString(),
-      ...payload,
-    });
+  async contactCoach(context, payload) {
+    const newRequest = { ...payload };
+    try {
+      const response = await fetch(
+        `https://vuejs-http-96326.firebaseio.com/requests/${payload.coachId}.json`,
+        {
+          method: 'POST',
+          body: JSON.stringify(newRequest),
+        }
+      );
+
+      newRequest.id = response.name;
+      context.commit('addRequest', newRequest);
+    } catch (e) {
+      throw new Error(e.message || 'Failed to send request');
+    }
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    try {
+      const response = await fetch(
+        `https://vuejs-http-96326.firebaseio.com/requests/${coachId}.json`
+      );
+      const responseData = await response.json();
+      context.commit(
+        'setRequests',
+        Object.keys(responseData).map((key) => {
+          return responseData[key];
+        })
+      );
+    } catch (e) {
+      throw new Error(e.message || 'Failed to fetch requests');
+    }
   },
 };
