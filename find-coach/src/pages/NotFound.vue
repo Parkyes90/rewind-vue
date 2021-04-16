3fba5b135b245736994559d83f8ed802819c3ab6
parkyes90 <parkyes90@gmail.com>
2021-04-16 23:08:03 +0900

    feature: Adding not found page

--- find-coach/src/pages/NotFound.vue
+++ find-coach/src/pages/NotFound.vue
@@ -1,3 +1,16 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>This page could not be found - maybe check out all our</p>
+      <router-link to="/coaches">coaches</router-link>
+    </base-card>
+  </section>
+</template>
+
 <script>
-export default {};
+import BaseCard from '@/components/ui/BaseCard';
+export default {
+  components: { BaseCard },
+};
 </script>
