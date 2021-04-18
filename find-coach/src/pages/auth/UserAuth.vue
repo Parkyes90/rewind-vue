e1ff5797af5be5eb0c7ec84a5dd5167570e3531f
parkyes90 <parkyes90@gmail.com>
2021-04-18 20:28:12 +0900

    feature: Add Loading spinner

--- find-coach/src/pages/auth/UserAuth.vue
+++ find-coach/src/pages/auth/UserAuth.vue
@@ -1,37 +1,49 @@
 <template>
-  <base-card>
-    <form @submit.prevent="submitForm">
-      <div class="form-control">
-        <label for="email">EMail</label>
-        <input type="email" id="email" v-model.trim="email" />
-      </div>
-      <div class="form-control">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model.trim="password" />
-      </div>
-      <p v-if="!formIsValid">
-        Please enter a valid email and password (must be at least 6 characters
-        long),
-      </p>
-      <base-button>{{ submitButtonCaption }}</base-button>
-      <base-button type="button" mode="flat" @click="switchAuthMode">{{
-        switchModeButtonCaption
-      }}</base-button>
-    </form>
-  </base-card>
+  <div>
+    <base-dialog :show="!!error" title="An Error occurred" @close="handleError">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+      <base-spinner></base-spinner>
+    </base-dialog>
+    <base-card>
+      <form @submit.prevent="submitForm">
+        <div class="form-control">
+          <label for="email">EMail</label>
+          <input type="email" id="email" v-model.trim="email" />
+        </div>
+        <div class="form-control">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model.trim="password" />
+        </div>
+        <p v-if="!formIsValid">
+          Please enter a valid email and password (must be at least 6 characters
+          long),
+        </p>
+        <base-button>{{ submitButtonCaption }}</base-button>
+        <base-button type="button" mode="flat" @click="switchAuthMode">{{
+          switchModeButtonCaption
+        }}</base-button>
+      </form>
+    </base-card>
+  </div>
 </template>
 <script>
 import BaseButton from '@/components/ui/BaseButton';
 import BaseCard from '@/components/ui/BaseCard';
 import { AUTH_MODE } from '@/constants/auth';
+import BaseDialog from '@/components/ui/BaseDialog';
+import BaseSpinner from '@/components/ui/BaseSpinner';
 export default {
-  components: { BaseCard, BaseButton },
+  components: { BaseDialog, BaseCard, BaseButton, BaseSpinner },
   data() {
     return {
       email: '',
       password: '',
       formIsValid: true,
       mode: AUTH_MODE.LOGIN.value,
+      isLoading: false,
+      error: null,
     };
   },
   computed: {
@@ -45,19 +57,32 @@ export default {
     },
   },
   methods: {
-    submitForm() {
+    handleError() {
+      this.error = null;
+    },
+    validateForm() {
       if (
         !this.email ||
         !this.email.includes('@') ||
         this.password.length < 6
       ) {
         this.formIsValid = false;
-      } else {
-        this.$store.dispatch('signUp', {
-          email: this.email,
-          password: this.password,
-        });
       }
+    },
+    async submitForm() {
+      this.isLoading = true;
+      this.validateForm();
+      if (this.formIsValid) {
+        try {
+          await this.$store.dispatch('signUp', {
+            email: this.email,
+            password: this.password,
+          });
+        } catch (e) {
+          this.error = e.message || 'Failed to authenticate, try later.';
+        }
+      }
+      this.isLoading = false;
     },
     switchAuthMode() {
       if (this.mode === AUTH_MODE.LOGIN.value) {
