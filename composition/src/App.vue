cbddbc0b2377220d7e5cd3865c0fe17e87977e86
parkyes90 <parkyes90@gmail.com>
2021-04-21 23:15:25 +0900

    feat: ref with input & apply computed

--- composition/src/App.vue
+++ composition/src/App.vue
@@ -2,11 +2,17 @@
   <section class="container">
     <h2>{{ user.name }}</h2>
     <h2>{{ user.age }}</h2>
+    <button @click="setNewAge">Set Age</button>
+    <div>
+      <input type="text" @input="setFirstName" />
+      <input type="text" @input="setLastName" />
+    </div>
+    <div>name: {{ name }}</div>
   </section>
 </template>
 
 <script>
-import { reactive } from 'vue';
+import { ref, reactive, computed } from 'vue';
 
 export default {
   setup() {
@@ -14,13 +20,26 @@ export default {
       name: 'parkyes90',
       age: 31,
     });
+    const firstName = ref('');
+    const lastName = ref('');
 
-    setTimeout(() => {
-      user.name = 'parkyes81';
+    const setNewAge = () => {
       user.age = 32;
-    }, 2000);
+    };
 
-    return { user };
+    const name = computed(() => {
+      return `${firstName.value} ${lastName.value}`;
+    });
+
+    const setFirstName = (event) => {
+      firstName.value = event.target.value;
+    };
+
+    const setLastName = (event) => {
+      lastName.value = event.target.value;
+    };
+
+    return { user, setNewAge, setFirstName, setLastName, name };
   },
 };
 </script>
